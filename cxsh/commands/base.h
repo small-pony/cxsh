87212f353cc3f08dd8e08a2c2b69be67e930efb2
small-pony <2262713487@qq.com>
2020-05-13 11:23:34 +0800

    add native

--- cxsh/commands/base.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-namespace commands
-{
-	extern void version();
-
-} // namespace commands
--- cxsh/cxsh.cpp
+++ cxsh/cxsh.cpp
@@ -3,11 +3,9 @@
 //4bdhdictrak6sqbqa6klcws3gh2nownzuznpnj5fon6etror4xoq
 #include "cxsh.h"
 
-#include "commands/base.h"
 
 using namespace std;
-int main(int argc, char *argv_raw[]) {
-  commands::version();
+int main(int argc, char *argv[]) {
   cout << "#" << endl;
   return 0;
 }
